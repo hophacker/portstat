90e516cb7cea6769a9b4a981a6dc6f752dd4a311
hophacker <jiefeng.hopkins@gmail.com>
2016-11-03 17:23:26 +0800

    version 0.0.8

--- portstat/portstat.py
+++ portstat/portstat.py
@@ -22,7 +22,7 @@ except ImportError:
     import ConfigParser as configparser
 
 __author__ = 'imlonghao'
-__version__ = '0.0.6'
+__version__ = '0.0.8'
 
 
 def version():
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='portstat',
-    version='0.0.7.5',
+    version='0.0.8',
     keywords=('port', 'monitor', 'traffic'),
     url='https://github.com/imlonghao/portstat',
     license='Apache License 2.0',
